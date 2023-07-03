4a37ac3dd63c6e131e25d9e1d02b915439ceef4a
imaksim <m596363@gmail.com>
2023-07-03 18:10:45 +0700

    add  debuger

--- calc/urls.py
+++ calc/urls.py
@@ -1,9 +1,16 @@
-from django.urls import path
+import debug_toolbar
+from django.urls import include, path
 from calc import views
+
 
 urlpatterns = [
     path("product/", views.ProductsListView.as_view(), name="products_view"),
-    path("product-create-view/", views.ProductCreateAndListView.as_view(), name="products_create_view")
-
+    path("product-create-view/", views.ProductCreateAndListView.as_view(), name="products_create_view"),
+    path("product-create-form/", views.ProductCreateView.as_view(), name="products_create_form"),
+    path('_debug/', include(debug_toolbar.urls)),
 
 ]
+
+
+
+
--- calc/views.py
+++ calc/views.py
@@ -4,17 +4,19 @@ from django.views.generic.edit import CreateView
 from calc.models import Stuff, Products, PaymentMethods,Operations,Sales
 from .forms import ProductForm
 # Create your views here.
+
 class ProductsListView (ListView):
     model = Products
-    template_name = 'calc/products.html'
+    template_name = 'calc/products_create_form.html'
     context_object_name = 'products'
+
 
 class ProductCreateView(CreateView):
     model = Products
     form_class = ProductForm
-    template_name = 'calc/products.html'
+    template_name = 'calc/product_create_form.html'
     context_object_name = 'product_create'
-    success_url = '/products/'
+    success_url = reverse_lazy('products_create_form')
 
 
 class ProductCreateAndListView(CreateView, ListView):
@@ -28,7 +30,7 @@ class ProductCreateAndListView(CreateView, ListView):
         print(request.POST)
         if 'selected_item' in request.POST:
             selected_items = request.POST.getlist('selected_item')
-
+            Products.objects.filter(id__in=selected_items).delete()
 
         return super().post(request, *args, **kwargs)
 
